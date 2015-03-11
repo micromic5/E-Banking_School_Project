94be17a0179600ed190a9476f54785961b1af44a
TheCell <simon_hischier@hotmail.com>
2015-03-11 15:04:50 +0100

    Changed to PDO

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,5 @@
 <?php
 
-	$host = "localhost";
-	$user = "root";
-	$pwd = "";
-	$database = "db_ebanking";
-
-	$con = mysqli_connect($host, $user, $pwd, $database);
-
-	if (mysqli_connect_errno())
-	{
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
+	$db = new PDO("mysql:host=localhost;dbname=db_schweiz", "root", "");
+	
 ?>
