1cba00cae5f68cb5d1ad342159a56fdf5cb9f2ce
TheCell <simon_hischier@hotmail.com>
2015-03-25 16:10:41 +0100

    Errorhandling db_connect
    added Errorhandling for the DB_Connect file. Outputs the Errormessage
    now.

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,7 +1,19 @@
 <?php
 
-	$db = new mysqli("localhost", "root", "", "db_ebanking");
-	$dbPDO = new PDO("mysql:host=localhost;dbname=db_ebanking","root","");
+	@$db = new mysqli("localhost", "root", "", "db_ebanking");
+
+	if (mysqli_connect_errno()) {
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+	}
+
+	try {
+		$dbPDO = new PDO("mysql:host=localhost;dbname=db_ebanking","root","");
+	} catch (PDOException $Exception) {
+
+		throw new MyDatabaseException($Exception->getMessage(), $Exception->getCode());
+		exit();
+	}
 
 	define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
 	
