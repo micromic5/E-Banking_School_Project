824bd0c11a1c563cedd9e4afe60796ab4ee95c85
micromic5 <mike.amacker@gmx.ch>
2015-03-25 13:42:24 +0100

    PDO
    Added PDO Connection

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,6 +1,7 @@
 <?php
 
 	$db = new mysqli("localhost", "root", "", "db_ebanking");
+	$dbPDO = new PDO("mysql:host=localhost;dbname=db_ebanking","root","");
 
 	define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
 	
